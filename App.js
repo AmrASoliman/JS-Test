3727aa687265b413f5444b724a02755a288cbf2a
Amr Soliman <soliman.amr@gmail.com>
2018-07-23 19:10:38 +0200

    add contact and profile

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
 
 mongoose.connect("mongodb://localhost:27017/employeedb");
 
@@ -9,6 +11,25 @@ var employeeSchema = mongoose.Schema({
 });
 
 var Employee = mongoose.model('Employee', employeeSchema);
+
+
+// Routes
+app.set('view engine', 'ejs');
+app.get("/", (req, res) => {
+      res.render('index')
+   });
+app.get("/Contact", (req, res) => {
+         res.render('Contact')
+      });
+app.get("/profile/:name", (req, res) => {
+        var data = {age: 42, job: 'ninja',hobbies: ['eating', 'fighting', 'fishing']};
+         res.render('profile',{person: req.params.name, data: data});
+      });
+
+// Listen
+app.listen(3000, () => {
+   console.log('Server listing on 3000');
+});
 /*
 //Add a Record
 var AmrSoliman = new Employee({
